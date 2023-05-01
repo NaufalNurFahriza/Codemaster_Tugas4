30a30a8fabb1153a8f768a4287151de3b0087a5e
Naufal Nurfahriza <123190024@student.upnyk.ac.id>
2023-05-02 05:20:42 +0700

    initial commit

---
+++ src/screen/SplashScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SplashScreen = ({}) => {
+    return (
+        <View>
+            <Text> ini SplashScreen </Text>
+        </View>
+    )
+}
+export default SplashScreen;
